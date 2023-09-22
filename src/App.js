8ea29892134b720b2dc101c71db3f59c5a140fb0
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2023-09-22 16:30:06 +0400

    Started working on navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import CustomerTable from "./components/customers/CustomerTable"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavbarElements from "./components/NavBar/NavBarComponents";
+import './App.css'
 
 function App() {
   return (
     <div className="container">
-    <CustomerTable />
+      <BrowserRouter>
+      <NavbarElements/>
+        <Routes>
+          <Route path="/" element={<CustomerTable />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/NavBar/NavBarComponents.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NavbarElements () {
+    return (
+        <div className="navBar-container">
+            <h1>Xelo</h1>
+        </div>
+    )
+}
